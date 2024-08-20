f0b5af9bb1510ffeab165f653090574dbff2c9c6
Arthur <117268493+Frightness@users.noreply.github.com>
2024-08-20 17:13:58 +0300

    first commit

---
+++ src/main/java/Task1/Task1.java
@@ -0,0 +1,30 @@
+package Task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+
+        System.out.println("Елементи масиву:");
+        for (int i : array) {
+            System.out.println(i);
+        }
+
+
+        int count = 0;
+        for (int i : array) {
+            if (i % 2 != 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Кількість непарних чисел: " + count);
+    }
+}
+
+
+// V 13: Int Arrays
+// Objective: To understand and use arrays with the int type in Java.
+// Instructions:
+// Declare and initialize an array of int.
+// Use a for loop to print the elements of the array to the console.
+// Use a for loop to find the number of odd in the array.
