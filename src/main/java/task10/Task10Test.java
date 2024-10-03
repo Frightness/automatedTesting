da1590358617597d471f87c55f3c7a365f622424
Arthur <117268493+Frightness@users.noreply.github.com>
2024-10-03 09:01:53 +0300

    completed task 9

--- src/main/java/task10/Task10Test.java
+++ src/main/java/task10/Task10Test.java
@@ -1,20 +1,35 @@
 package task10;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Task10Test {
-    public static void main(String[] args) {
-        WebDriver driver;
-        @BeforeTest
-        void setUp() {
-            ChromeDriverManager.getInstance().setup;
-            System.setProperty("webdriver.chrome.driver",
-                    "driver/chromedriver");
 
-            driver = new ChromeDriver();
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        // Налаштування ChromeDriver за допомогою WebDriverManager
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testTask() {
+        driver.get("https://demoqa.com/buttons");
+
+        // Ваші дії з елементами на сторінці
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
         }
-
-
     }
 }
